f99af504e4558717a5fbf455c509928ac7736806
Chris022 <christoph02.cw@gmail.com>
2020-10-14 19:29:40 +0200

    Add files via upload

---
+++ precache-manifest.743375e0b526eea2bab644bc9037a31a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7942cd1a51ce5fe302dffd939296ef58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16bba6d9954b1814803e",
+    "url": "/static/js/2.c1eb6b7a.chunk.js"
+  },
+  {
+    "revision": "556d72da1c19c1e88e2917fa6505d7f0",
+    "url": "/static/js/2.c1eb6b7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99b378d2eeac80361908",
+    "url": "/static/js/main.7b951ca9.chunk.js"
+  },
+  {
+    "revision": "14b1b51d3b6eda25a07a",
+    "url": "/static/js/runtime-main.ab3bc54c.js"
+  }
+]);
